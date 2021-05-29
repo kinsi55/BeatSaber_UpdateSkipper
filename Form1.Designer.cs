46185b2d822316827f8a1e87acc95497db867631
Kinsi <kinsi55@users.noreply.github.com>
2021-05-29 23:01:16 +0200

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace BeatSaberNoUpdate {
+	partial class Form1 {
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textbox_path = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+			this.textbox_manifest = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+			this.button3 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(135, 15);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Beat Saber install Folder:";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(288, 31);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Browse...";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textbox_path
+			// 
+			this.textbox_path.Location = new System.Drawing.Point(12, 31);
+			this.textbox_path.Name = "textbox_path";
+			this.textbox_path.PlaceholderText = "Please select...";
+			this.textbox_path.Size = new System.Drawing.Size(270, 23);
+			this.textbox_path.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 66);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(159, 15);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "Latest Beat Saber Manifest ID";
+			// 
+			// linkLabel1
+			// 
+			this.linkLabel1.AutoSize = true;
+			this.linkLabel1.Location = new System.Drawing.Point(177, 66);
+			this.linkLabel1.Name = "linkLabel1";
+			this.linkLabel1.Size = new System.Drawing.Size(100, 15);
+			this.linkLabel1.TabIndex = 6;
+			this.linkLabel1.TabStop = true;
+			this.linkLabel1.Text = "(Get it from Here)";
+			this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+			// 
+			// textbox_manifest
+			// 
+			this.textbox_manifest.Location = new System.Drawing.Point(12, 84);
+			this.textbox_manifest.MaxLength = 19;
+			this.textbox_manifest.Name = "textbox_manifest";
+			this.textbox_manifest.PlaceholderText = "19 digit number...";
+			this.textbox_manifest.Size = new System.Drawing.Size(351, 23);
+			this.textbox_manifest.TabIndex = 7;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 148);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(159, 23);
+			this.button2.TabIndex = 8;
+			this.button2.Text = "Apply";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(15, 174);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(304, 15);
+			this.label3.TabIndex = 9;
+			this.label3.Text = "Created by Kinsi. Check out my Mods they\'re kinda nice:";
+			// 
+			// linkLabel2
+			// 
+			this.linkLabel2.AutoSize = true;
+			this.linkLabel2.Location = new System.Drawing.Point(325, 174);
+			this.linkLabel2.Name = "linkLabel2";
+			this.linkLabel2.Size = new System.Drawing.Size(30, 15);
+			this.linkLabel2.TabIndex = 10;
+			this.linkLabel2.TabStop = true;
+			this.linkLabel2.Text = "#AD";
+			this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(204, 148);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(159, 23);
+			this.button3.TabIndex = 11;
+			this.button3.Text = "What will this do?";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Checked = true;
+			this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.checkBox1.Location = new System.Drawing.Point(12, 113);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(217, 19);
+			this.checkBox1.TabIndex = 12;
+			this.checkBox1.Text = "Disable Autoupdate if its not already";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(375, 198);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.linkLabel2);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textbox_manifest);
+			this.Controls.Add(this.linkLabel1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textbox_path);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Beat Saber Update Skipper (Steam Only)";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textbox_path;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.TextBox textbox_manifest;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeatSaberNoUpdate {
+	public partial class Form1 : Form {
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			using(var dialog = new FolderBrowserDialog()) {
+				if(dialog.ShowDialog() != DialogResult.OK)
+					return;
+
+				textbox_path.Text = dialog.SelectedPath;
+			}
+		}
+
+		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+			MessageBox.Show("Copy the latest 'Manifest ID' from the site. Make sure that 'Last update' looks correct, to make sure the site has already spotted the update!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+			Process.Start(new ProcessStartInfo("https://steamdb.info/depot/620981/manifests") {
+				UseShellExecute = true,
+				Verb = "open"
+			});
+		}
+
+		private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+			Process.Start(new ProcessStartInfo("https://github.com/kinsi55?tab=repositories&q=BeatSaber") {
+				UseShellExecute = true,
+				Verb = "open"
+			});
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			try {
+				var p = Registry.GetValue(
+					@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App 620980",
+					"InstallLocation", 
+					null
+				);
+
+				if(p != null && CheckFolderPath((string)p))
+					textbox_path.Text = (string)p;
+			} catch { }
+		}
+
+		bool CheckFolderPath(string path) {
+			if(!Directory.Exists(path))
+				return false;
+
+			if(!File.Exists(Path.Join(path, "..", "..", "appmanifest_620980.acf")))
+				return false;
+
+			return true;
+		}
+
+		void Bad(string str) {
+			MessageBox.Show(str, "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+		}
+
+		void SetKv(ref string input, string key, string val) {
+			input = Regex.Replace(input, $"\"{key}\".*", $"\t\"{key}\"\t\"{val}\"", RegexOptions.IgnoreCase);
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			if(!CheckFolderPath(textbox_path.Text)) {
+				Bad("It seems like the Folder you selected is incorrect. You can go to the properties of Beat Saber in Steam and click on 'Browse Game files' for an easy method to get the correct path");
+				return;
+			}
+
+			if(!Regex.IsMatch(textbox_manifest.Text, "[0-9]{19}")) {
+				Bad("The Manifest ID you entered is incorrect. It should be a 19 digit number. Make sure to not accidently enter any spaces");
+				return;
+			}
+
+			if(Process.GetProcesses().Any(x => x.ProcessName.ToLower().StartsWith("steam"))) {
+				Bad("Steam seems to be running, please exit it to apply the patch");
+				return;
+			}
+
+			var p = Path.Join(textbox_path.Text, "..", "..", "appmanifest_620980.acf");
+
+			var acf = File.ReadAllText(p);
+
+			if(acf.Contains(textbox_manifest.Text))
+				Bad("Seems like this update is already applied. I'll apply it again for good measure");
+
+			SetKv(ref acf, "ScheduledAutoUpdate", "0");
+			SetKv(ref acf, "LastUpdated", ((uint)DateTimeOffset.UnixEpoch.ToUnixTimeSeconds()).ToString());
+			SetKv(ref acf, "StateFlags", "4");
+			SetKv(ref acf, "UpdateResult", "0");
+
+			if(checkBox1.Checked)
+				SetKv(ref acf, "AutoUpdateBehavior", "1");
+
+			acf = Regex.Replace(acf, "(\"620981\".*?\"manifest\".*)\"[0-9]{19}\"", $"$1\"{textbox_manifest.Text}\"", RegexOptions.Singleline | RegexOptions.IgnoreCase);
+
+			File.WriteAllText(p, acf);
+
+			MessageBox.Show("Patch applied. If everything worked correctly Steam should not require you to update Beat Saber any more the next time you start it", "Success");
+		}
+
+		private void button3_Click(object sender, EventArgs e) {
+			MessageBox.Show(
+				"This tool modifies a config file of Steam to make it think that you already have the last version (Indicated by the correct Manifest ID) eventho you are not.\n" +
+				"\n" +
+				"You will need to redo this whenever an Update is released (Basically whenever Steam prompts you to Update to start the game you use this tool instead to fake that you did update"
+			);
+		}
+	}
+}

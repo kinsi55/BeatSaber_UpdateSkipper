e43ce99ff8c1e832a88cf6c3ec2518bcee4669e5
nakami <nakami@users.noreply.github.com>
2021-07-23 02:33:20 +0200

    Improve wording and fix typos

--- Form1.cs
+++ Form1.cs
@@ -70,17 +70,17 @@ namespace BeatSaberNoUpdate {
 
 		private void applyButton_Click(object sender, EventArgs e) {
 			if(!CheckFolderPath(textbox_path.Text)) {
-				Bad("It seems like the Folder you selected is incorrect. You can go to the properties of Beat Saber in Steam and click on 'Browse Game files' for an easy method to get the correct path");
+				Bad("It seems like the selected folder is incorrect. You can go to the properties of Beat Saber in Steam and click on 'Browse Game files' for an easy method to get the correct path.");
 				return;
 			}
 
 			if(!Regex.IsMatch(textbox_manifest.Text, "[0-9]{19}")) {
-				Bad("The Manifest ID you entered is incorrect. It should be a 19 digit number. Make sure to not accidently enter any spaces");
+				Bad("It seems like the entered Manifest ID is incorrect. It's supposed to be a 19-digit number. Make sure not to enter any spaces by accident.");
 				return;
 			}
 
 			if(Process.GetProcesses().Any(x => x.ProcessName.ToLower() == "steam")) {
-				Bad("Steam seems to be running, please exit it to apply the patch");
+				Bad("Steam seems to be already running, please exit it to apply the patch.");
 				return;
 			}
 
@@ -89,7 +89,7 @@ namespace BeatSaberNoUpdate {
 			var acf = File.ReadAllText(p);
 
 			if(acf.Contains(textbox_manifest.Text))
-				Bad("Seems like this update is already applied. I'll apply it again for good measure");
+				Bad("Seems like this update is already applied. It will now be applied again for good measure.");
 
 			SetKv(ref acf, "ScheduledAutoUpdate", "0");
 			SetKv(ref acf, "LastUpdated", ((uint)DateTimeOffset.Now.ToUnixTimeSeconds()).ToString());
@@ -103,14 +103,14 @@ namespace BeatSaberNoUpdate {
 
 			File.WriteAllText(p, acf);
 
-			MessageBox.Show("Patch applied. If everything worked correctly Steam should most likely not ask you to update Beat Saber any more. If it still does its probably related to DLC(Assmung you have any) and should not update the game, just to be sure I advise you to backup your game version", "Success");
+			MessageBox.Show("Patch applied. If everything worked out correctly, Steam should most likely not ask you to update Beat Saber anymore. If it still does, this may be related to DLC (assuming you have any) and should not update the game. Be sure to backup your game version.", "Success");
 		}
 
 		private void aboutButton_Click(object sender, EventArgs e) {
 			MessageBox.Show(
-				"This tool modifies a config file of Steam to make it think that you already have the last version (Indicated by the correct Manifest ID) eventho you are not.\n" +
+				"This tool modifies a configuration file of Steam to spoof the version Saber Version to be the latest without actually updating your local version.\n" +
 				"\n" +
-				"You will need to redo this whenever an Update is released (Basically whenever Steam prompts you to Update to start the game you use this tool instead to fake that you did update"
+				"You will need to run this tool again whenever an update for Beat Saber is released. Basically, whenever Steam prompts you to update the game prior to starting it, you will need to use this tool first and spoof the latest version."
 			);
 		}
 
@@ -124,7 +124,7 @@ namespace BeatSaberNoUpdate {
 			if(manifest != null) {
 				textbox_manifest.Text = manifest;
             } else {
-				MessageBox.Show("Automatically retreiving the Manifest ID failed. Copy the latest 'Manifest ID' from the site. Make sure that 'Last update' looks correct, to confirm the site has already spotted the latest update!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				MessageBox.Show("Retrieving the Manifest ID failed. Copy the latest 'Manifest ID' from the website. Make sure that 'Last update' looks correct, to confirm the site has already spotted the latest update!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
 				LaunchUrl($"https://steamdb.info/depot/{AppInfo.DEPOT_ID}/manifests");
 			}
 
